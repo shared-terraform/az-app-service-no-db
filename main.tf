5dd76d0ab7194d6354923205dfe4e13cc13a3a6c
TorrentialFire <wilsontfwi@gmail.com>
2020-09-04 17:54:24 -0500

    Fix resource group name declaration to use EOT string

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
 
+# We follow Microsoft's naming and tagging conventions to create our resources.
+# Refer to the documentation, here:
+# https://docs.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/naming-and-tagging
+
+# To avoid collisions of combinations of plain strings for various resources 
+# which require either subscription or globally unique names, we generate a GUID
+# in every instance of Terraform state that uses this module.
 resource "random_uuid" "uuid" { }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.az_app_name}-${var.az_deploy_env}${terraform.workspace == "default" ? "" : "-${terraform.workspace}"}-${random_uuid.uuid.result}"
+  name = <<-EOT
+    rg-
+    ${var.az_app_name}-
+    ${var.az_deploy_env}
+    ${terraform.workspace == "default" ? "" : "-${terraform.workspace}"}-
+    ${random_uuid.uuid.result}
+    EOT
+  
   location = var.az_location
   tags     = var.az_tags
 }
+
