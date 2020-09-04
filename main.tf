a3edd5e0d87dcb83d66317fc976383c49a274955
TorrentialFire <wilsontfwi@gmail.com>
2020-09-04 17:22:28 -0500

    Fix incorrectly referenced variable

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "random_uuid" "uuid" { }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.az_app_name}${terraform.workspace == "default" ? "" : "-${terraform.workspace}"}-${var.deploy_env}"
+  name     = "rg-${var.az_app_name}${terraform.workspace == "default" ? "" : "-${terraform.workspace}"}-${var.az_deploy_env}"
   location = var.az_location
   tags     = var.az_tags
 }
