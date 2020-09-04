d3755a0a820317333f24e76def6137ee9d95912f
TorrentialFire <wilsontfwi@gmail.com>
2020-09-04 17:39:12 -0500

    Fix the workspace and deploy env order and add uuid to rg

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "random_uuid" "uuid" { }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.az_app_name}${terraform.workspace == "default" ? "" : "-${terraform.workspace}"}-${var.az_deploy_env}"
+  name     = "rg-${var.az_app_name}-${var.az_deploy_env}${terraform.workspace == "default" ? "" : "-${terraform.workspace}"}-${random_uuid.uuid.result}"
   location = var.az_location
   tags     = var.az_tags
 }
