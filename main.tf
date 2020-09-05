416421c5a4881fc7d6c14d6ac4b7ad298049fc92
TorrentialFire <wilsontfwi@gmail.com>
2020-09-04 22:14:43 -0500

    Deploy an app service with custom hostname

--- main.tf
+++ main.tf
@@ -8,16 +8,69 @@
 # in every instance of Terraform state that uses this module.
 resource "random_uuid" "uuid" { }
 
-resource "azurerm_resource_group" "rg" {
-  name = <<-EOT
-    rg-
-    ${var.az_app_name}-
-    ${var.az_deploy_env}-
-    %{ if terraform.workspace != "default" }${terraform.workspace}-%{endif}
-    ${random_uuid.uuid.result}
-    EOT
+# Create a local variable that holds the a composite of the application name,
+# deployment environment, and current workspace name (if it exists).
+locals {
+  composite_name = "${var.az_app_name}-${var.az_deploy_env}${terraform.workspace == "default" ? "" : "-${terraform.workspace}"}"
   
+  reserved = var.az_asp_reserved || var.az_asp_kind == "Linux" ? true : false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${local.composite_name}-${random_uuid.uuid.result}"
+
   location = var.az_location
   tags     = var.az_tags
 }
 
+resource "azurerm_app_service_plan" "asp" {
+  name                = "asp-${local.composite_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  kind                = var.az_asp_kind
+  # When creating a Linux App Service Plan, reserved must be set to true!
+  reserved            = local.reserved
+  # maximum_elastic_worker_count
+  # app_service_environment_id
+  # per_site_scaling
+  tags                = var.az_tags
+
+  sku {
+    tier = var.az_asp_sku_tier
+    size = var.az_asp_sku_size
+    # capacity
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name = "app-${local.composite_name}-${random_uuid.uuid.result}"
+  resource_group_name     = azurerm_resource_group.rg.name
+  location                = azurerm_resource_group.rg.location
+  app_service_plan_id     = azurerm_app_service_plan.asp.id
+  client_affinity_enabled = var.az_app_client_affinity_enabled
+  client_cert_enabled     = var.az_app_client_cert_enabled
+  enabled                 = var.az_app_enabled
+  https_only              = var.az_app_https_only
+  tags                    = var.az_tags
+
+  # app_settings {}
+  # auth_settings {}
+  # backup {}
+  # connection_string {}
+  # identity {}
+  # logs {}
+  # storage_accout {}
+  # site_config {
+    # cors {}
+
+  #   ftps_state = "Disabled"
+  # }
+  # source_control {}
+}
+
+resource "azurerm_app_service_custom_hostname_binding" "custom_domain" {
+  count               = length(var.az_custom_domain_hostname)
+  hostname            = var.az_custom_domain_hostname[count.index]
+  app_service_name    = azurerm_app_service.app.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
--- variables.tf
+++ variables.tf
@@ -6,6 +6,11 @@ variable "az_app_name" {
   description = "The name of the application for which this infrastructure is being deployed."
 }
 
+variable "az_tags" {
+  type = map(string)
+  description = "Tags which are applied to all resources created using this module."
+}
+
 # Configurable variables
 
 variable "az_location" {
@@ -20,7 +25,55 @@ variable "az_deploy_env" {
   default = "dev"
 }
 
-variable "az_tags" {
-  type = map(string)
-  description = "Tags which are applied to all resources created using this module."
+variable "az_asp_kind" {
+  type = string
+  description = "The type of apps to be deployed into this app service plan (Windows, Linux, elastic, and FunctionApp)."
+  default = "Linux"
+}
+
+variable "az_asp_reserved" {
+  type = bool
+  default = false
+}
+
+variable "az_asp_sku_tier" {
+  type = string
+  description = "The pricing tier to use in Azure for the app service plan (Free, Basic, Standard, Premium, and Isolated)."
+  default = "Free"
+}
+
+variable "az_asp_sku_size" {
+  type = string
+  description = "The size of the app service plan based on the tier (Free: F1; Basic: B1, B2, B3; Standard: S1, S2, S3; Premium: P1v2, P2v2, P3v2; Isolated: I1, I2, I3)."
+  default = "F1"
+}
+
+variable "az_app_client_affinity_enabled" {
+  type = bool
+  description = "Should the App Service send session affinity cookies, which route client requests in the same session to the same instance?"
+  default = false
+}
+
+variable "az_app_client_cert_enabled" {
+  type = bool
+  description = "Does the App Service require client certificates for incoming requests?"
+  default = false
+}
+
+variable "az_app_enabled" {
+  type = bool
+  description = "Should the app service be enabled immediately?"
+  default = true
+}
+
+variable "az_app_https_only" {
+  type = bool
+  description = "Should Azure enforce HTTPS traffic only (rather than the app itself)?"
+  default = false
+}
+
+variable "az_custom_domain_hostname" {
+  type = list(string)
+  description = "A list of custom hostnames to apply to the app."
+  default = []
 }
